53210c819eec1b38f0c9823c1228478c2d962c11
kirdua <kirdua@gmail.com>
2024-06-17 09:45:05 -0500

    implement route guards and username not showing

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,14 +2,14 @@ const routes = [
   {
     path: '/',
     name: 'overview',
-    component: () => import('@/views/overview/Dashboard.vue')
-    // beforeEnter(to, from, next) {
-    //   next()
-    // }
-    // meta: {
-    //   requiresAuth: true,
-    //   header: 'Overview'
-    // }
+    component: () => import('@/views/overview/Dashboard.vue'),
+    beforeEnter(to, from, next) {
+      next()
+    },
+    meta: {
+      requiresAuth: true,
+      header: 'Overview'
+    }
   },
   {
     path: '/progress',
@@ -17,11 +17,11 @@ const routes = [
     component: () => import('@/views/progress/ProgressReport.vue'),
     beforeEnter(to, from, next) {
       next()
+    },
+    meta: {
+      requiresAuth: true,
+      header: 'Annual Progress Report'
     }
-    // meta: {
-    //   requiresAuth: true,
-    //   header: 'Annual Progress Report'
-    // }
   },
   {
     path: '/login',
@@ -36,62 +36,62 @@ const routes = [
   {
     path: '/children',
     name: 'children',
-    component: () => import('@/views/children/Children.vue')
-    // beforeEnter(to, from, next) {
-    //   next()
-    // }
-    // meta: {
-    //   requiresAuth: true,
-    //   header: 'Children Profiles'
-    // }
+    component: () => import('@/views/children/Children.vue'),
+    beforeEnter(to, from, next) {
+      next()
+    },
+    meta: {
+      requiresAuth: true,
+      header: 'Children Profiles'
+    }
   },
   {
     path: '/goals',
     name: 'goals',
-    component: () => import('@/views/goals/Goals.vue')
-    // beforeEnter(to, from, next) {
-    //   next()
-    // }
-    // meta: {
-    //   requiresAuth: true,
-    //   header: 'Goals'
-    // }
+    component: () => import('@/views/goals/Goals.vue'),
+    beforeEnter(to, from, next) {
+      next()
+    },
+    meta: {
+      requiresAuth: true,
+      header: 'Goals'
+    }
   },
   {
     path: '/scheduled-services',
     name: 'scheduled-services',
-    component: () => import('@/views/services/scheduled/ScheduledServices.vue')
-    // beforeEnter(to, from, next) {
-    //   next()
-    // }
-    // meta: {
-    //   requiresAuth: true,
-    //   header: 'Scheduled Services'
-    // }
+    component: () => import('@/views/services/scheduled/ScheduledServices.vue'),
+    beforeEnter(to, from, next) {
+      next()
+    },
+    meta: {
+      requiresAuth: true,
+      header: 'Scheduled Services'
+    }
   },
   {
     path: '/add-services',
     name: 'add-services',
-    component: () => import('@/views/services/add/AddServices.vue')
-    // beforeEnter(to, from, next) {
-    //   next()
-    // }
-    // meta: {
-    //   requiresAuth: true,
-    //   header: 'Add Scheduled Services'
-    // }
+    component: () => import('@/views/services/add/AddServices.vue'),
+    beforeEnter(to, from, next) {
+      next()
+    },
+    meta: {
+      requiresAuth: true,
+      header: 'Add Scheduled Services'
+    }
   },
   {
     path: '/other-services',
     name: 'other-services',
-    component: () => import('@/views/other/OtherServices.vue')
-    // beforeEnter(to, from, next) {
-    //   next()
-    // }
-    // meta: {
-    //   requiresAuth: true,
-    //   header: 'Other Services'
-    // }
+    component: () => import('@/views/other/OtherServices.vue'),
+    beforeEnter(to, from, next) {
+      next()
+    },
+    meta: {
+      requiresAuth: true,
+      header: 'Other Services'
+    }
   },
   {
     path: '/:catchAll(.*)*',
