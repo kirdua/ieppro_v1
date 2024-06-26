129da2fc4196018cafda444a667b5813c4d16cdf
kirdua <kirdua@gmail.com>
2024-06-26 10:44:39 -0500

    created goal modal and basic functionality

--- src/views/goals/add/AddGoalsDialog.vue
+++ src/views/goals/add/AddGoalsDialog.vue
@@ -1,14 +1,35 @@
 <script setup>
 import { ref } from 'vue'
 import useGoalsStore from '@/stores/goals'
+import { progressGradedByOptions } from '@/constants'
 
 const goalsStore = useGoalsStore()
 const goalTypeItems = ['Academic', 'Functional', 'Related Services']
 
 const goalFocus = ref('')
-const goalType = ref('' || goalTypeItems[0])
-const numberOfBenchmarks = ref(1)
+const goalType = ref(goalTypeItems[0])
+const benchmarks = ref(['']) // Initialize with one empty benchmark
+const currentImplementer = ref(progressGradedByOptions[1])
+
+const addBenchmark = () => {
+  benchmarks.value.push('')
+}
+
+const removeBenchmark = (index) => {
+  benchmarks.value.splice(index, 1)
+}
+
+const closeGoalsModal = () => {
+  goalsStore.modalIsVisible = false
+  goalFocus.value = ''
+  goalType.value = goalTypeItems[0]
+  benchmarks.value = []
+  currentImplementer.value = progressGradedByOptions[1]
+}
+
+const addGoal = () => {}
 </script>
+
 <template>
   <v-dialog v-model="goalsStore.modalIsVisible" max-width="600">
     <v-card prepend-icon="mdi-check-circle" class="text-blue" title="Goals">
@@ -18,7 +39,7 @@ const numberOfBenchmarks = ref(1)
             <v-text-field
               label="Goal"
               required
-              v-model="goal"
+              v-model="goalFocus"
               placeholder="Math, Reading, etc..."
             ></v-text-field>
           </v-col>
@@ -40,42 +61,29 @@ const numberOfBenchmarks = ref(1)
             <v-text-field label="Duration"></v-text-field>
           </v-col>
 
-          <v-col cols="12" md="12" sm="12" v-for="num in numberOfBenchmarks" :key="num">
-            <v-text-field label="Benchmark" append-inner-icon="mdi-close-circle"></v-text-field>
+          <v-col cols="12" md="12" sm="12" v-for="(benchmark, index) in benchmarks" :key="index">
+            <v-text-field
+              v-model="benchmarks[index]"
+              label="Benchmark"
+              append-inner-icon="mdi-close-circle"
+              @click:append-inner="removeBenchmark(index)"
+            ></v-text-field>
           </v-col>
 
           <v-col cols="12" md="12" sm="12">
             <div class="d-flex justify-end">
-              <v-btn @click="numberOfBenchmarks++">Add Benchmark</v-btn>
+              <v-btn @click="addBenchmark">Add Benchmark</v-btn>
             </div>
           </v-col>
 
-          <v-col cols="12" sm="6">
-            <v-select :items="['0-17', '18-29', '30-54', '54+']" label="Age*" required></v-select>
-          </v-col>
-
-          <v-col cols="12" sm="6">
+          <v-col cols="12" sm="12">
             <v-autocomplete
-              :items="[
-                'Skiing',
-                'Ice hockey',
-                'Soccer',
-                'Basketball',
-                'Hockey',
-                'Reading',
-                'Writing',
-                'Coding',
-                'Basejump'
-              ]"
-              label="Interests"
-              auto-select-first
-              multiple
+              v-model="currentImplementer"
+              :items="progressGradedByOptions"
+              label="Implementer"
             ></v-autocomplete>
           </v-col>
         </v-row>
-
-        <small class="text-caption text-medium-emphasis">*indicates required field</small>
-        {{ goalType }}
       </v-card-text>
 
       <v-divider></v-divider>
@@ -83,9 +91,14 @@ const numberOfBenchmarks = ref(1)
       <v-card-actions>
         <v-spacer></v-spacer>
 
-        <v-btn text="Close" variant="plain" @click="dialog = false"></v-btn>
+        <v-btn text="Close" variant="plain" @click="closeGoalsModal"></v-btn>
 
-        <v-btn color="primary" text="Save" variant="tonal" @click="dialog = false"></v-btn>
+        <v-btn
+          color="primary"
+          text="Save"
+          variant="tonal"
+          @click="goalsStore.modalIsVisible = false"
+        ></v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
