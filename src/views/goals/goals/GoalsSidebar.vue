1ca22dcb9a85903c99f87b485339352b24e9b416
kirdua <kirdua@gmail.com>
2024-08-09 12:20:40 -0500

    implementing adding new benchmarks to existing benchmarks

--- src/views/goals/goals/GoalsSidebar.vue
+++ src/views/goals/goals/GoalsSidebar.vue
@@ -1,8 +1,33 @@
 <script setup>
+import { ref } from 'vue'
+import { toast } from 'vue3-toastify'
 import useGoalsStore from '@/stores/goals'
 
 const goalsStore = useGoalsStore()
+
+const newBenchmarks = ref([])
+
+const addBenchmark = () => {
+  newBenchmarks.value.push('')
+}
+
+const removeBenchmark = (index) => {
+  newBenchmarks.value.splice(index, 1)
+}
+
+const updateGoals = () => {
+  // goalsStore.selectedGoalRow.benchmarks.push(...newBenchmarks.value)
+
+  try {
+    goalsStore.updateGoalsByGradeLevel(...newBenchmarks.value)
+    newBenchmarks.value = []
+    toast.success('Updated benckmarks')
+  } catch (error) {
+    toast.success('Failed to add benchmarks')
+  }
+}
 </script>
+
 <template>
   <v-navigation-drawer
     v-model="goalsStore.showGoalsSidebar"
@@ -15,8 +40,9 @@ const goalsStore = useGoalsStore()
     <v-list-item nav class="custom-background">
       <template v-slot:append>
         <v-btn
-          icon="mdi-close"
-          variant="text"
+          icon="mdi-close-thick"
+          variant="plain"
+          color="blue-1"
           @click.stop="goalsStore.toggleGoalsDrawer(false)"
         ></v-btn>
       </template>
@@ -67,9 +93,29 @@ const goalsStore = useGoalsStore()
       <v-list-item>
         <v-list-item-content>
           <v-list-item-title>
-            <strong>Benchmarks:</strong>
+            <v-row class="d-flex align-center">
+              <v-col>
+                <h4>Benchmarks:</h4>
+              </v-col>
+              <v-col cols="auto">
+                <v-tooltip text="Add benchmark">
+                  <template v-slot:activator="{ props }">
+                    <v-btn
+                      v-bind="props"
+                      color="blue-1"
+                      icon="mdi mdi-plus-thick"
+                      variant="plain"
+                      @click="addBenchmark"
+                    >
+                    </v-btn>
+                  </template>
+                </v-tooltip>
+              </v-col>
+            </v-row>
           </v-list-item-title>
+
           <v-list>
+            <!-- Existing Benchmarks -->
             <v-list-item
               v-for="(benchmark, index) in goalsStore.selectedGoalRow.benchmarks"
               :key="index"
@@ -78,10 +124,26 @@ const goalsStore = useGoalsStore()
                 <v-list-item-title>{{ index + 1 }}. {{ benchmark }}</v-list-item-title>
               </v-list-item-content>
             </v-list-item>
+
+            <!-- New Benchmarks -->
+            <v-list-item v-for="(benchmark, index) in newBenchmarks" :key="'new-' + index">
+              <v-list-item-content>
+                <v-text-field
+                  v-model="newBenchmarks[index]"
+                  variant="outlined"
+                  :append-inner-icon="'mdi-delete'"
+                  append-icon-cb
+                  @click:append-inner="removeBenchmark(index)"
+                ></v-text-field>
+              </v-list-item-content>
+            </v-list-item>
           </v-list>
         </v-list-item-content>
       </v-list-item>
     </v-list>
+    <v-list-item v-if="newBenchmarks.length > 0">
+      <v-btn color="blue-1" block @click="updateGoals"> Submit </v-btn>
+    </v-list-item>
   </v-navigation-drawer>
 </template>
 
